6e650dad60fc68b29298df8348da306d947bff01
YuChih Hu <hijac.hu@gmail.com>
2019-12-03 22:23:00 +0800

    Rendering a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pepper';
+  cuisines$: Observable<any[]>;
+
+  constructor(private af: AngularFireDatabase) {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.cuisines$ = this.af.list('/cuisines').snapshotChanges();
+  }
 }
