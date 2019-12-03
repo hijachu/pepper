1942ff131de04030dbadae56e06390693e374630
YuChih Hu <hijac.hu@gmail.com>
2019-12-03 22:13:32 +0800

    Initialize AngularFireModule and  firebase config.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAz506R_VYg4PgwNWeaL6AsGuxWNL9q_jw",
+    authDomain: "pepper-ac11b.firebaseapp.com",
+    databaseURL: "https://pepper-ac11b.firebaseio.com",
+    projectId: "pepper-ac11b",
+    storageBucket: "pepper-ac11b.appspot.com",
+    messagingSenderId: "629579378675",
+    appId: "1:629579378675:web:dd05e2be5de7f0c7162c86"
+  }
+
 };
