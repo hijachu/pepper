547becb24bba0ea95c0224cae49401c8c0d24f05
YuChih Hu <hijac.hu@gmail.com>
2019-12-08 14:06:39 +0800

    Joining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,80 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   title = 'pepper';
+
+  cuisines: AngularFireList<any>;
   cuisines$: Observable<any[]>;
+
+  restaurant: AngularFireObject<any>;
   restaurant$: Observable<any>;
 
-  constructor(private af: AngularFireDatabase) {
+  restaurants: AngularFireList<any>;
+  restaurants$: Observable<any[]>;
 
+  constructor(private af: AngularFireDatabase) {
   }
 
   ngOnInit(): void {
-    this.cuisines$ = this.af.list('/cuisines').snapshotChanges();
-    this.restaurant$ = this.af.object('/restaurant').valueChanges();
+    this.cuisines = this.af.list('/cuisines');
+    this.cuisines$ = this.cuisines.snapshotChanges();
+
+    this.restaurant = this.af.object('/restaurant');
+    this.restaurant$ = this.restaurant.valueChanges();
+
+    this.restaurants = this.af.list('/restaurants');
+    this.restaurants$ = this.restaurants.valueChanges()
+      .pipe(
+        map(restaurants => {
+          console.log("BEFORE MAP", restaurants);
+
+          const restaurants_new = restaurants.map(restaurant =>
+            ({ cuisineType: (this.af.object('/cuisines/' + restaurant.cuisine).valueChanges()),
+              ...restaurant })
+          );
+
+          console.log("AFTER MAP", restaurants_new);
+          return restaurants_new;
+        })
+      );
+
+
+/*
+    this.restaurants$ = this.restaurants.snapshotChanges()
+      .pipe(
+        map(restaurants => {
+          console.log("BEFORE MAP", restaurants);
+          restaurants.map(restaurant => {
+            console.log(restaurant);
+            // console.log(restaurant.payload.val());
+            console.log('/cuisines/' + restaurant.payload.val().cuisine);
+            restaurant.payload.val().cuisineType = this.af.object('/cuisines/' + restaurant.payload.val().cuisine);
+            console.log(restaurant.payload.val());
+          });
+          return restaurants;
+        })
+      )
+*/
   }
+
+  add() {
+    this.cuisines.push({
+      name: 'Asian'
+    });
+  }
+
+  update() {
+    this.af.object('/restaurant').update({
+      name: 'New Name',
+      rating: 5
+    });
+
+    // this.af.object('/favorite/1/10').set(true);
+  }
+
+  remove() {
+    this.af.object('/restaurant').remove()
+      .then(x => console.log("SUCCESS"))
+      .catch(error => console.log("ERROR ", error));
+  }
+
 }
